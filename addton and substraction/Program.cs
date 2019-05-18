2b51f516586b3e3aa6f83bfa5fb8eb206dbb0058
ouhmad kamal <ouhmad.kamal@hotmail.com>
2019-05-18 16:25:46 -0400

    Initial version

---
+++ addton and substraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addton_and_substraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using different mathematical operations 
+            int a = 10;
+            int b = 15;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.ReadLine();
+        }
+    }
+}
